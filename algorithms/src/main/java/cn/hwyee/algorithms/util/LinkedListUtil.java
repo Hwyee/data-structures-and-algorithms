730a88e077d740a8bdf65d8316ad3af9fc388e87
hwyee <hwyee@foxmail.com>
2023-06-01 00:14:47 +0800

    链表中倒数最后k个结点方法实现 双指针快慢指针

--- algorithms/src/main/java/cn/hwyee/algorithms/util/LinkedListUtil.java
+++ algorithms/src/main/java/cn/hwyee/algorithms/util/LinkedListUtil.java
@@ -292,4 +292,32 @@ public class LinkedListUtil {
         return slow;
     }
 
+
+    /**
+     * 链表中倒数最后k个结点
+     *
+     *
+     * @param pHead ListNode类
+     * @param k int整型
+     * @return ListNode类
+     */
+    public ListNode findKthToTail (ListNode pHead, int k) {
+        int n = 0;
+        ListNode fast = pHead;
+        ListNode slow = pHead;
+        //快指针先行k步
+        for (int i = 0; i < k; i++) {
+            if (fast != null)
+                fast = fast.next;
+                //达不到k步说明链表过短，没有倒数k
+            else
+                return slow = null;
+        }
+        //快慢指针同步，快指针先到底，慢指针指向倒数第k个
+        while (fast != null) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+        return slow;
+    }
 }
