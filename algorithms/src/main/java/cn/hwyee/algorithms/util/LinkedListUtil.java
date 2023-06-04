bdcb54adca87ab3e83ad6dc0b08c867279c7ad8d
hwyee <hwyee@foxmail.com>
2023-06-04 14:13:53 +0800

    链表的奇偶重排方法实现

--- algorithms/src/main/java/cn/hwyee/algorithms/util/LinkedListUtil.java
+++ algorithms/src/main/java/cn/hwyee/algorithms/util/LinkedListUtil.java
@@ -493,4 +493,74 @@ public class LinkedListUtil {
         }
         return true;
     }
+
+    /**
+     * 链表的奇偶重排
+     * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+     * 194ms
+     * 占用内存
+     * 25568KB
+     * @param head ListNode类
+     * @return ListNode类
+     */
+    public ListNode oddEvenListMy (ListNode head) {
+        // write code here
+        int i = 1;
+        ListNode odd = new ListNode(-1);
+        ListNode even = new ListNode(-1);
+        ListNode oddt = odd;
+        ListNode event = even;
+        while(head!=null){
+            if(i%2==0){
+                event.next=head;
+                event=event.next;
+            } else {
+                oddt.next = head;
+                oddt=oddt.next;
+            }
+            head=head.next;
+            i++;
+        }
+        event.next=null;
+        oddt.next=even.next;
+        return odd.next;
+    }
+
+    /**
+     * oddEvenList:
+     * 运行时间
+     * 226ms
+     * 占用内存
+     * 26104KB
+     * @author hui
+     * @version 1.0
+     * @param head  
+     * @return cn.hwyee.datastructures.linkedlist.ListNode
+     * @date 2023/6/4 14:13
+     */
+    public ListNode oddEvenList (ListNode head) {
+        //如果链表为空，不用重排
+        if(head == null)
+            return head;
+        //even开头指向第二个节点，可能为空
+        ListNode even = head.next;
+        //odd开头指向第一个节点
+        ListNode odd = head;
+        //指向even开头
+        ListNode evenhead = even;
+        while(even != null && even.next != null){
+            //odd连接even的后一个，即奇数位
+            odd.next = even.next;
+            //odd进入后一个奇数位
+            odd = odd.next;
+            //even连接后一个奇数的后一位，即偶数位
+            even.next = odd.next;
+            //even进入后一个偶数位
+            even = even.next;
+        }
+        //even整体接在odd后面
+        odd.next = evenhead;
+        return head;
+    }
+
 }
