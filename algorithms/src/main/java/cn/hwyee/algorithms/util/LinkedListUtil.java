f74e3046dd7775f5d070adb32c55a23c4f5aa102
hwyee <hwyee@foxmail.com>
2023-06-09 00:12:33 +0800

    比较版本号 方法实现

--- algorithms/src/main/java/cn/hwyee/algorithms/util/LinkedListUtil.java
+++ algorithms/src/main/java/cn/hwyee/algorithms/util/LinkedListUtil.java
@@ -165,7 +165,7 @@ public class LinkedListUtil {
         if (left > right) {
             return null;
         }
-            //中间一个的情况
+        //中间一个的情况
         else if (left == right) {
             return lists.get(left);
         }
@@ -332,7 +332,7 @@ public class LinkedListUtil {
             if (fast != null) {
                 fast = fast.next;
             }
-                //达不到k步说明链表过短，没有倒数k
+            //达不到k步说明链表过短，没有倒数k
             else {
                 return slow = null;
             }
@@ -401,6 +401,7 @@ public class LinkedListUtil {
 
     /**
      * 链表相加(二)
+     *
      * @param head1 ListNode类
      * @param head2 ListNode类
      * @return ListNode类
@@ -457,38 +458,40 @@ public class LinkedListUtil {
      * 304ms
      * 占用内存
      * 27384KB
+     *
      * @param head ListNode类 the head
      * @return bool布尔型
      */
-    public boolean isPailMy (ListNode head) {
+    public boolean isPailMy(ListNode head) {
         // write code here
-        if(head.next == null) {
+        if (head.next == null) {
             return true;
         }
         int i = 0;
         ListNode tmp = head;
         Stack<ListNode> stack = new Stack<>();
-        while (tmp != null){
+        while (tmp != null) {
             stack.push(tmp);
             tmp = tmp.next;
             i++;
         }
         for (int j = 0; j < i / 2; j++) {
-            if (head.val!=stack.pop().val) {
+            if (head.val != stack.pop().val) {
                 return false;
             }
-            head=head.next;
+            head = head.next;
         }
         return true;
     }
 
     /**
      * 判断一个链表是否为回文结构 双指针
+     *
      * @param head ListNode类 the head
      * @return bool布尔型
      */
     public boolean isPail(ListNode head) {
-        ListNode q= head, p= head;
+        ListNode q = head, p = head;
         //通过快慢指针找到中点
         while (q != null && q.next != null) {
             q = q.next.next;
@@ -519,29 +522,30 @@ public class LinkedListUtil {
      * 194ms
      * 占用内存
      * 25568KB
+     *
      * @param head ListNode类
      * @return ListNode类
      */
-    public ListNode oddEvenListMy (ListNode head) {
+    public ListNode oddEvenListMy(ListNode head) {
         // write code here
         int i = 1;
         ListNode odd = new ListNode(-1);
         ListNode even = new ListNode(-1);
         ListNode oddt = odd;
         ListNode event = even;
-        while(head!=null){
-            if(i%2==0){
-                event.next=head;
-                event=event.next;
+        while (head != null) {
+            if (i % 2 == 0) {
+                event.next = head;
+                event = event.next;
             } else {
                 oddt.next = head;
-                oddt=oddt.next;
+                oddt = oddt.next;
             }
-            head=head.next;
+            head = head.next;
             i++;
         }
-        event.next=null;
-        oddt.next=even.next;
+        event.next = null;
+        oddt.next = even.next;
         return odd.next;
     }
 
@@ -551,15 +555,16 @@ public class LinkedListUtil {
      * 226ms
      * 占用内存
      * 26104KB
+     *
+     * @param head
+     * @return cn.hwyee.datastructures.linkedlist.ListNode
      * @author hui
      * @version 1.0
-     * @param head  
-     * @return cn.hwyee.datastructures.linkedlist.ListNode
      * @date 2023/6/4 14:13
      */
-    public ListNode oddEvenList (ListNode head) {
+    public ListNode oddEvenList(ListNode head) {
         //如果链表为空，不用重排
-        if(head == null) {
+        if (head == null) {
             return head;
         }
         //even开头指向第二个节点，可能为空
@@ -568,7 +573,7 @@ public class LinkedListUtil {
         ListNode odd = head;
         //指向even开头
         ListNode evenhead = even;
-        while(even != null && even.next != null){
+        while (even != null && even.next != null) {
             //odd连接even的后一个，即奇数位
             odd.next = even.next;
             //odd进入后一个奇数位
@@ -586,29 +591,31 @@ public class LinkedListUtil {
     /**
      * 删除有序链表中重复的元素-I
      * 删除给出链表中的重复元素（链表中元素从小到大有序），使链表中的所有元素都只出现一次
+     *
      * @param head ListNode类
      * @return ListNode类
      */
-    public ListNode deleteDuplicates (ListNode head) {
+    public ListNode deleteDuplicates(ListNode head) {
         //空链表
-        if(head == null) {
+        if (head == null) {
             return null;
         }
         //遍历指针
         ListNode cur = head;
         //指针当前和下一位不为空
-        while(cur != null && cur.next != null){
+        while (cur != null && cur.next != null) {
             //如果当前与下一位相等则忽略下一位
-            if(cur.val == cur.next.val) {
+            if (cur.val == cur.next.val) {
                 cur.next = cur.next.next;
             }
-                //否则指针正常遍历
+            //否则指针正常遍历
             else {
                 cur = cur.next;
             }
         }
         return head;
     }
+
     /**
      * 给出一个升序排序的链表，删除链表中的所有重复出现的元素，只保留原链表中只出现一次的元素。
      * 可以跳过多个相同的值。
@@ -616,28 +623,28 @@ public class LinkedListUtil {
      * 74ms
      * 占用内存
      * 12896KB
+     *
      * @param head ListNode类
      * @return ListNode类
      */
-    public ListNode deleteDuplicates2 (ListNode head) {
+    public ListNode deleteDuplicates2(ListNode head) {
         //空链表
-        if(head == null) {
+        if (head == null) {
             return null;
         }
         ListNode res = new ListNode(0);
         //在链表前加一个表头
         res.next = head;
         ListNode cur = res;
-        while(cur.next != null && cur.next.next != null){
+        while (cur.next != null && cur.next.next != null) {
             //遇到相邻两个节点值相同
-            if(cur.next.val == cur.next.next.val){
+            if (cur.next.val == cur.next.next.val) {
                 int temp = cur.next.val;
                 //将所有相同的都跳过
                 while (cur.next != null && cur.next.val == temp) {
                     cur.next = cur.next.next;
                 }
-            }
-            else {
+            } else {
                 cur = cur.next;
             }
         }
@@ -649,11 +656,11 @@ public class LinkedListUtil {
      * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
      * 二分查找-I
      *
-     * @param nums int整型一维数组
+     * @param nums   int整型一维数组
      * @param target int整型
      * @return int整型
      */
-    public int search (int[] nums, int target) {
+    public int search(int[] nums, int target) {
         // write code here
         if (nums.length == 0) {
             return -1;
@@ -681,14 +688,15 @@ public class LinkedListUtil {
      * find:二维数组中的查找 线性搜索 利用二维数组行列递增特性
      * 在一个二维数组array中（每个一维数组的长度相同），每一行都按照从左到右递增的顺序排序，
      * 每一列都按照从上到下递增的顺序排序。请完成一个函数，输入这样的一个二维数组和一个整数，判断数组中是否含有该整数。
-     * @author hui
-     * @version 1.0
+     *
      * @param target
      * @param array
      * @return boolean
+     * @author hui
+     * @version 1.0
      * @date 2023/6/5 23:39
      */
-    public boolean find(int target, int [][] array) {
+    public boolean find(int target, int[][] array) {
         //优先判断特殊
         if (array.length == 0) {
             return false;
@@ -704,7 +712,7 @@ public class LinkedListUtil {
             if (array[i][j] > target) {
                 i--;
             }
-                //元素较小，往右走
+            //元素较小，往右走
             else if (array[i][j] < target) {
                 j++;
             } else {
@@ -719,43 +727,44 @@ public class LinkedListUtil {
      * 解题思路： 利用数组行列递增特性。
      * 主要思路：一维的二分查找可以舍弃一半的查找范围，二维的二分可以舍弃左上部分或者右下部分1/4的查找范围。
      * public:
-     *     bool double_binary(vector<vector<int>> arr,int x1,int x2,int y1, int y2,int target){
-     *         if(x1 == x2 || y1 == y2) return false;
-     *         int xmid = (x1+x2)/2, ymid = (y1+y2)/2;
-     *         int num = arr[xmid][ymid];
-     *         if(num == target) return true;
-     *         if(num > target)
-     *         {
-     *            if(double_binary(arr, x1, xmid, y1, y2, target)) return true;
-     *            if(double_binary(arr,xmid,x2,y1,ymid,target)) return true;
-     *         }
-     *         else
-     *         {
-     *             if(double_binary(arr, xmid+1, x2, y1, y2, target)) return true;
-     *             if(double_binary(arr, x1, xmid+1, ymid+1, y2, target)) return true;
-     *         }
-     *         return false;
-     *     }
-     *     bool Find(int target, vector<vector<int> > array) {
-     *         if(array.size() == 0) return false;
-     *         return double_binary(array, 0, array.size(), 0, array[0].size(), target);
-     *     }
-     *
+     * bool double_binary(vector<vector<int>> arr,int x1,int x2,int y1, int y2,int target){
+     * if(x1 == x2 || y1 == y2) return false;
+     * int xmid = (x1+x2)/2, ymid = (y1+y2)/2;
+     * int num = arr[xmid][ymid];
+     * if(num == target) return true;
+     * if(num > target)
+     * {
+     * if(double_binary(arr, x1, xmid, y1, y2, target)) return true;
+     * if(double_binary(arr,xmid,x2,y1,ymid,target)) return true;
+     * }
+     * else
+     * {
+     * if(double_binary(arr, xmid+1, x2, y1, y2, target)) return true;
+     * if(double_binary(arr, x1, xmid+1, ymid+1, y2, target)) return true;
+     * }
+     * return false;
+     * }
+     * bool Find(int target, vector<vector<int> > array) {
+     * if(array.size() == 0) return false;
+     * return double_binary(array, 0, array.size(), 0, array[0].size(), target);
+     * }
      */
-    public boolean find1(int target, int [][] array) {
+    public boolean find1(int target, int[][] array) {
         return false;
     }
+
     /**
-     *  寻找峰值
+     * 寻找峰值
      * 给定一个长度为n的数组，返回其中任何一个峰值的索引
      * 峰值元素是指其值严格大于左右相邻值的元素
      * 数组两个边界可以看成是最小，nums[−1]=nums[n]=−∞
      * 峰值不存在平的情况，即相邻元素不会相等
      * 因为题目将数组边界看成最小值，而我们只需要找到其中一个波峰，因此只要不断地往高处走，一定会有波峰。
+     *
      * @param nums int整型一维数组
      * @return int整型
      */
-    public int findPeakElement (int[] nums) {
+    public int findPeakElement(int[] nums) {
         // write code here
         int left = 0;
         int right = nums.length - 1;
@@ -766,7 +775,7 @@ public class LinkedListUtil {
             if (nums[mid] > nums[mid + 1]) {
                 right = mid;
             }
-                //右边是往上，一定能找到波峰
+            //右边是往上，一定能找到波峰
             else {
                 left = mid + 1;
             }
@@ -775,10 +784,10 @@ public class LinkedListUtil {
         return right;
     }
 
-    public int mergeSort(int left, int right, int [] data, int [] temp){
+    public int mergeSort(int left, int right, int[] data, int[] temp) {
         int mod = 1000000007;
         //停止划分
-        if(left >= right) {
+        if (left >= right) {
             return 0;
         }
         //取中间
@@ -788,17 +797,17 @@ public class LinkedListUtil {
         //防止溢出
         res %= mod;
         int i = left, j = mid + 1;
-        for(int k = left; k <= right; k++) {
+        for (int k = left; k <= right; k++) {
             temp[k] = data[k];
         }
-        for(int k = left; k <= right; k++){
-            if(i == mid + 1) {
+        for (int k = left; k <= right; k++) {
+            if (i == mid + 1) {
                 data[k] = temp[j++];
-            } else if(j == right + 1 || temp[i] <= temp[j]) {
+            } else if (j == right + 1 || temp[i] <= temp[j]) {
                 data[k] = temp[i++];
             }
-                //左边比右边大，答案增加
-            else{
+            //左边比右边大，答案增加
+            else {
                 data[k] = temp[j++];
                 // 统计逆序对
                 res += mid - i + 1;
@@ -806,22 +815,165 @@ public class LinkedListUtil {
         }
         return res % mod;
     }
-    
+
     /**
      * inversePairs:
      * 数组中的逆序对
      * 如果前面一个数字大于后面的数字，则这两个数字组成一个逆序对。
      * 输入一个数组，求一个数组的全部逆序对，答案对1000000007取模
      * 题目保证输入的数组中没有的相同的数字
+     *
+     * @param array
+     * @return int
      * @author hui
      * @version 1.0
-     * @param array  
-     * @return int
      * @date 2023/6/6 23:53
      */
-    public int inversePairs(int [] array) {
+    public int inversePairs(int[] array) {
         int n = array.length;
         int[] res = new int[n];
         return mergeSort(0, n - 1, array, res);
+    }
+
+    /**
+     * minNumberInRotateArray:
+     * 旋转数组的最小数字
+     * 有一个长度为 n 的非降序数组，比如[1,2,3,4,5]，将它进行旋转，即把一个数组最开始的若干个元素搬到数组的末尾，
+     * 变成一个旋转数组，比如变成了[3,4,5,1,2]，或者[4,5,1,2,3]这样的。请问，给定这样一个旋转数组，求数组中的最小值。
+     * 运行时间
+     * 75ms
+     * 占用内存
+     * 12936KB
+     *
+     * @param array
+     * @return int
+     * @author hui
+     * @version 1.0
+     * @date 2023/6/7 23:56
+     */
+    public int minNumberInRotateArray(int[] array) {
+        if (array == null) {
+            return -1;
+        }
+
+        for (int i = 0; i < array.length - 1; i++) {
+            if (array[i] > array[i + 1]) {
+                return array[i + 1];
+            }
+        }
+        return array[0];
+    }
+
+
+    /**
+     * 版本号是由修订号组成，修订号与修订号之间由一个"."连接。
+     * 1个修订号可能有多位数字组成，修订号可能包含前导0，且是合法的。例如，1.02.11，0.1，0.2都是合法的版本号
+     * 每个版本号至少包含1个修订号。
+     * 修订号从左到右编号，下标从0开始，最左边的修订号下标为0，下一个修订号下标为1，以此类推。
+     * <p>
+     * 比较规则：
+     * 一. 比较版本号时，请按从左到右的顺序依次比较它们的修订号。比较修订号时，只需比较忽略任何前导零后的整数值。
+     * 比如"0.1"和"0.01"的版本号是相等的
+     * 二. 如果版本号没有指定某个下标处的修订号，则该修订号视为0。例如，"1.1"的版本号小于"1.1.1"。
+     * 因为"1.1"的版本号相当于"1.1.0"，第3位修订号的下标为0，小于1
+     * 三.  version1 > version2 返回1，如果 version1 < version2 返回-1，不然返回0.
+     * 运行时间
+     * 26ms
+     * 占用内存
+     * 10096KB
+     * 比较版本号
+     *
+     * @param version1 string字符串
+     * @param version2 string字符串
+     * @return int整型
+     */
+    public int compare(String version1, String version2) {
+        // write code here
+        String[] a1 = version1.split("\\.");
+        String[] a2 = version2.split("\\.");
+        int f1 = findFirstNotZero(a1);
+        int f2 = findFirstNotZero(a2);
+        int max = Math.max(a1.length, a2.length);
+        int v1 = 0;
+        int v2 = 0;
+        boolean isFirst = false;
+
+        for (int i = 0; i < max; i++) {
+            if (i == 0) {
+                v1 = Integer.parseInt(a1[i]);
+            } else if ((i + f1) > (a1.length - 1)) {
+                v1 = 0;
+            } else {
+                v1 = Integer.parseInt(a1[i + f1]);
+            }
+            if (i == 0) {
+                v2 = Integer.parseInt(a2[i]);
+            } else if ((i + f2) > (a2.length - 1)) {
+                v2 = 0;
+            } else {
+                v2 = Integer.parseInt(a2[i + f2]);
+            }
+            if (v1 > v2) {
+                return 1;
+            } else if (v1 < v2) {
+                return -1;
+            }
+        }
+        return 0;
+    }
+
+    public int findFirstNotZero(String[] s) {
+        for (int i = 1; i < s.length; i++) {
+            if (Integer.parseInt(s[i]) != 0) {
+                return i - 1;
+            }
+        }
+        return 0;
+    }
+
+    /**
+     * compareGF:
+     * 官方 双指针遍历截取
+     * 运行时间
+     * 20ms
+     * 占用内存
+     * 9800KB
+     * @author hui
+     * @version 1.0
+     * @param version1
+     * @param version2
+     * @return int
+     * @date 2023/6/9 0:11
+     */
+    public int compareGF(String version1, String version2) {
+        int n1 = version1.length();
+        int n2 = version2.length();
+        int i = 0, j = 0;
+        //直到某个字符串结束
+        while (i < n1 || j < n2) {
+            long num1 = 0;
+            //从下一个点前截取数字
+            while (i < n1 && version1.charAt(i) != '.') {
+                num1 = num1 * 10 + (version1.charAt(i) - '0');
+                i++;
+            }
+            //跳过点
+            i++;
+            long num2 = 0;
+            //从下一个点前截取数字
+            while (j < n2 && version2.charAt(j) != '.') {
+                num2 = num2 * 10 + (version2.charAt(j) - '0');
+                j++;
+            }
+            //跳过点
+            j++;
+            //比较数字大小
+            if (num1 > num2)
+                return 1;
+            if (num1 < num2)
+                return -1;
+        }
+        //版本号相同
+        return 0;
     }
 }
