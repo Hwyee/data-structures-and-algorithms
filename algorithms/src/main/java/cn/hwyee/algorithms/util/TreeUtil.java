ad828e31e6983b9638f3d3683b0925c25ea7b0aa
hwyee <hwyee@foxmail.com>
2023-06-12 00:45:05 +0800

    二叉树的后序遍历 方法实现

--- algorithms/src/main/java/cn/hwyee/algorithms/util/TreeUtil.java
+++ algorithms/src/main/java/cn/hwyee/algorithms/util/TreeUtil.java
@@ -20,7 +20,8 @@ public class TreeUtil {
 
     /**
      * 二叉树的前序遍历
-     * 前序遍历首先访问根结点然后遍历左子树，最后遍历右子树。在遍历左、右子树时，仍然先访问根结点，然后遍历左子树，最后遍历右子树。
+     * 前序遍历首先访问根结点然后遍历左子树，最后遍历右子树。
+     * 在遍历左、右子树时，仍然先访问根结点，然后遍历左子树，最后遍历右子树。
      *
      * @param root TreeNode类
      * @return int整型一维数组
@@ -51,8 +52,9 @@ public class TreeUtil {
 
     public void inorder(List<Integer> list, TreeNode root){
         //遇到空节点则返回
-        if(root == null)
+        if(root == null) {
             return;
+        }
         //先去左子树
         inorder(list, root.left);
         //再访问根节点
@@ -78,8 +80,45 @@ public class TreeUtil {
         inorder(list, root);
         //返回的结果
         int[] res = new int[list.size()];
-        for(int i = 0; i < list.size(); i++)
+        for(int i = 0; i < list.size(); i++) {
             res[i] = list.get(i);
+        }
         return res;
+    }
+
+
+    /**
+     * 二叉树的后序遍历
+     * 什么是二叉树的后续遍历，简单来说就是“左右根”，
+     * 展开来说就是优先访问根节点的左子树的全部节点，然后再访问根节点的右子树的全部节点，最后再访问根节点。
+     * 对于每棵子树的访问也按照这个逻辑，因此叫做“左右根”的顺序。
+     *
+     * @param root TreeNode类
+     * @return int整型一维数组
+     */
+    public int[] postorderTraversal (TreeNode root) {
+        //添加遍历结果的数组
+        List<Integer> list = new ArrayList();
+        //递归中序遍历
+        inorder(list, root);
+        //返回的结果
+        int[] res = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            res[i] = list.get(i);
+        }
+        return res;
+    }
+
+    public void postorder(List<Integer> list, TreeNode root) {
+        //遇到空节点则返回
+        if (root == null) {
+            return;
+        }
+        //先去左子树
+        inorder(list, root.left);
+        //最后去右子树
+        inorder(list, root.right);
+        //再访问根节点
+        list.add(root.val);
     }
 }
