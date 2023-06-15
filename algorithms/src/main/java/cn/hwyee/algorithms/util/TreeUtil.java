0ea1a5e546df1c0ee5b00ecdf0f91eb201bd8f11
hwyee <hwyee@foxmail.com>
2023-06-15 23:39:13 +0800

    对称的二叉树、合并二叉树、二叉树的镜像、判断是不是二叉搜索树、 判断是不是完全二叉树方法实现

--- algorithms/src/main/java/cn/hwyee/algorithms/util/TreeUtil.java
+++ algorithms/src/main/java/cn/hwyee/algorithms/util/TreeUtil.java
@@ -28,9 +28,9 @@ public class TreeUtil {
      * @param root TreeNode类
      * @return int整型一维数组
      */
-    public int[] preorderTraversal (TreeNode root) {
+    public int[] preorderTraversal(TreeNode root) {
         ArrayList<TreeNode> list = new ArrayList<>();
-        traverse(list,root);
+        traverse(list, root);
         int[] ints = new int[list.size()];
         for (int i = 0; i < list.size(); i++) {
             ints[i] = list.get(i).val;
@@ -38,23 +38,23 @@ public class TreeUtil {
         return ints;
     }
 
-    public void traverse(List<TreeNode> list,TreeNode node){
+    public void traverse(List<TreeNode> list, TreeNode node) {
         if (node == null) {
-            return ;
+            return;
         }
         list.add(node);
         if (node.left != null) {
-            traverse(list,node.left);
+            traverse(list, node.left);
         }
         if (node.right != null) {
-            traverse(list,node.right);
+            traverse(list, node.right);
         }
     }
 
 
-    public void inorder(List<Integer> list, TreeNode root){
+    public void inorder(List<Integer> list, TreeNode root) {
         //遇到空节点则返回
-        if(root == null) {
+        if (root == null) {
             return;
         }
         //先去左子树
@@ -67,22 +67,23 @@ public class TreeUtil {
 
     /**
      * inorderTraversal:
-     *  二叉树的中序遍历
-     *  中序遍历就是从最左边开始，把每个节点垂直投影到同一直线上，然后从左往右读值
+     * 二叉树的中序遍历
+     * 中序遍历就是从最左边开始，把每个节点垂直投影到同一直线上，然后从左往右读值
+     *
+     * @param root
+     * @return int[]
      * @author hui
      * @version 1.0
-     * @param root  
-     * @return int[]
      * @date 2023/6/11 0:21
      */
-    public int[] inorderTraversal (TreeNode root) {
+    public int[] inorderTraversal(TreeNode root) {
         //添加遍历结果的数组
         List<Integer> list = new ArrayList();
         //递归中序遍历
         inorder(list, root);
         //返回的结果
         int[] res = new int[list.size()];
-        for(int i = 0; i < list.size(); i++) {
+        for (int i = 0; i < list.size(); i++) {
             res[i] = list.get(i);
         }
         return res;
@@ -98,7 +99,7 @@ public class TreeUtil {
      * @param root TreeNode类
      * @return int整型一维数组
      */
-    public int[] postorderTraversal (TreeNode root) {
+    public int[] postorderTraversal(TreeNode root) {
         //添加遍历结果的数组
         List<Integer> list = new ArrayList();
         //递归中序遍历
@@ -127,12 +128,13 @@ public class TreeUtil {
     /**
      * 二叉树的层序遍历
      * 从左到右，一层一层地遍历
+     *
      * @param root TreeNode类
-     * @return int整型ArrayList<ArrayList<>>
+     * @return int整型ArrayList<ArrayList <>>
      */
-    public ArrayList<ArrayList<Integer>> levelOrder (TreeNode root) {
+    public ArrayList<ArrayList<Integer>> levelOrder(TreeNode root) {
         ArrayList<ArrayList<Integer>> arrayLists = new ArrayList<>();
-        if (root==null) {
+        if (root == null) {
             return arrayLists;
         }
         Queue<TreeNode> queue = new LinkedList<>();
@@ -140,13 +142,13 @@ public class TreeUtil {
         while (!queue.isEmpty()) {
             ArrayList<Integer> list = new ArrayList<>();
             int size = queue.size();
-            for(int i = 0; i < size; i++) {
+            for (int i = 0; i < size; i++) {
                 TreeNode poll = queue.poll();
                 list.add(poll.val);
-                if (poll.left!=null){
+                if (poll.left != null) {
                     queue.add(poll.left);
                 }
-                if (poll.right!=null){
+                if (poll.right != null) {
                     queue.add(poll.right);
                 }
             }
@@ -156,22 +158,23 @@ public class TreeUtil {
     }
 
     /**
-     * print: 
+     * print:
      * 按之字形顺序打印二叉树
      * 第一层从左向右，下一层从右向左，一直这样交替
      * 运行时间
      * 28ms
      * 占用内存
      * 10272KB
+     *
+     * @param pRoot
+     * @return java.util.ArrayList<java.util.ArrayList < java.lang.Integer>>
      * @author hui
      * @version 1.0
-     * @param pRoot  
-     * @return java.util.ArrayList<java.util.ArrayList<java.lang.Integer>>
      * @date 2023/6/13 23:11
      */
-    public ArrayList<ArrayList<Integer> > print(TreeNode pRoot) {
+    public ArrayList<ArrayList<Integer>> print(TreeNode pRoot) {
         ArrayList<ArrayList<Integer>> arrayLists = new ArrayList<>();
-        if (pRoot==null) {
+        if (pRoot == null) {
             return arrayLists;
         }
         LinkedList<TreeNode> odd = new LinkedList<>();
@@ -179,19 +182,19 @@ public class TreeUtil {
         odd.add(pRoot);
         while (!odd.isEmpty() || !even.isEmpty()) {
             ArrayList<Integer> list = new ArrayList<>();
-            if (!odd.isEmpty()){
+            if (!odd.isEmpty()) {
                 int size = odd.size();
-                for(int i = 0; i < size; i++) {
+                for (int i = 0; i < size; i++) {
                     TreeNode poll = odd.poll();
                     list.add(poll.val);
-                    if (poll.left!=null){
+                    if (poll.left != null) {
                         even.add(poll.left);
                     }
-                    if (poll.right!=null){
+                    if (poll.right != null) {
                         even.add(poll.right);
                     }
                 }
-            }else {
+            } else {
                 int size = even.size();
                 for (TreeNode peek : even) {
                     if (peek.left != null) {
@@ -201,7 +204,7 @@ public class TreeUtil {
                         odd.add(peek.right);
                     }
                 }
-                for(int i = 0; i < size; i++) {
+                for (int i = 0; i < size; i++) {
                     TreeNode poll = even.pollLast();
                     list.add(poll.val);
                 }
@@ -217,17 +220,18 @@ public class TreeUtil {
      * 27ms
      * 占用内存
      * 10116KB
+     *
+     * @param pRoot
+     * @return java.util.ArrayList<java.util.ArrayList < java.lang.Integer>>
      * @author hui
      * @version 1.0
-     * @param pRoot  
-     * @return java.util.ArrayList<java.util.ArrayList<java.lang.Integer>>
      * @date 2023/6/13 23:53
      */
-    public ArrayList<ArrayList<Integer> > printOffice(TreeNode pRoot) {
+    public ArrayList<ArrayList<Integer>> printOffice(TreeNode pRoot) {
         TreeNode head = pRoot;
-        ArrayList<ArrayList<Integer> > res = new ArrayList<ArrayList<Integer>>();
+        ArrayList<ArrayList<Integer>> res = new ArrayList<ArrayList<Integer>>();
         if (head == null)
-            //如果是空，则直接返回空list
+        //如果是空，则直接返回空list
         {
             return res;
         }
@@ -270,17 +274,18 @@ public class TreeUtil {
      * 26ms
      * 占用内存
      * 9944KB
+     *
+     * @param pRoot
+     * @return java.util.ArrayList<java.util.ArrayList < java.lang.Integer>>
      * @author hui
      * @version 1.0
-     * @param pRoot
-     * @return java.util.ArrayList<java.util.ArrayList<java.lang.Integer>>
      * @date 2023/6/13 23:56
      */
-    public ArrayList<ArrayList<Integer> > printOfficeDoubleStack(TreeNode pRoot) {
+    public ArrayList<ArrayList<Integer>> printOfficeDoubleStack(TreeNode pRoot) {
         TreeNode head = pRoot;
-        ArrayList<ArrayList<Integer> > res = new ArrayList<ArrayList<Integer>>();
-        if(head == null)
-            //如果是空，则直接返回空list
+        ArrayList<ArrayList<Integer>> res = new ArrayList<ArrayList<Integer>>();
+        if (head == null)
+        //如果是空，则直接返回空list
         {
             return res;
         }
@@ -288,42 +293,42 @@ public class TreeUtil {
         Stack<TreeNode> s2 = new Stack<TreeNode>();
         //放入第一次
         s1.push(head);
-        while(!s1.isEmpty() || !s2.isEmpty()){
+        while (!s1.isEmpty() || !s2.isEmpty()) {
             ArrayList<Integer> temp = new ArrayList<Integer>();
             //遍历奇数层
-            while(!s1.isEmpty()){
+            while (!s1.isEmpty()) {
                 TreeNode node = s1.pop();
                 //记录奇数层
                 temp.add(node.val);
                 //奇数层的子节点加入偶数层
-                if(node.left != null) {
+                if (node.left != null) {
                     s2.push(node.left);
                 }
-                if(node.right != null) {
+                if (node.right != null) {
                     s2.push(node.right);
                 }
             }
             //数组不为空才添加
-            if(temp.size() != 0) {
+            if (temp.size() != 0) {
                 res.add(new ArrayList<Integer>(temp));
             }
             //清空本层数据
             temp.clear();
             //遍历偶数层
-            while(!s2.isEmpty()){
+            while (!s2.isEmpty()) {
                 TreeNode node = s2.pop();
                 //记录偶数层
                 temp.add(node.val);
                 //偶数层的子节点加入奇数层
-                if(node.right != null) {
+                if (node.right != null) {
                     s1.push(node.right);
                 }
-                if(node.left != null) {
+                if (node.left != null) {
                     s1.push(node.left);
                 }
             }
             //数组不为空才添加
-            if(temp.size() != 0) {
+            if (temp.size() != 0) {
                 res.add(new ArrayList<Integer>(temp));
             }
             //清空本层数据
@@ -338,24 +343,25 @@ public class TreeUtil {
      * 57ms
      * 占用内存
      * 12568KB
+     *
      * @param root TreeNode类
      * @return int整型
      */
-    public int maxDepth (TreeNode root) {
-        if (root==null) {
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
             return 0;
         }
         int maxDepth = 0;
         LinkedList<TreeNode> lists = new LinkedList<>();
         lists.add(root);
-        while(!lists.isEmpty()){
+        while (!lists.isEmpty()) {
             int size = lists.size();
-            for(int i = 0; i < size; i++){
+            for (int i = 0; i < size; i++) {
                 TreeNode poll = lists.poll();
-                if (poll.left != null){
+                if (poll.left != null) {
                     lists.add(poll.left);
                 }
-                if (poll.right != null){
+                if (poll.right != null) {
                     lists.add(poll.right);
                 }
             }
@@ -370,10 +376,10 @@ public class TreeUtil {
      * 递归是一个过程或函数在其定义或说明中有直接或间接调用自身的一种方法，
      * 它通常把一个大型复杂的问题层层转化为一个与原问题相似的规模较小的问题来求解。
      * 因此递归过程，最重要的就是查看能不能讲原本的问题分解为更小的子问题，这是使用递归的关键。
-     *
+     * <p>
      * 而二叉树的递归，则是将某个节点的左子树、右子树看成一颗完整的树，
      * 那么对于子树的访问或者操作就是对于原树的访问或者操作的子问题，因此可以自我调用函数不断进入子树。
-     *
+     * <p>
      * 思路：
      * 二叉树的深度就等于根节点这个1层加上左子树和右子树深度的最大值，即
      * rootDepth = max(leftDepth + rightDepth) + 1
@@ -385,15 +391,16 @@ public class TreeUtil {
      * 占用内存
      * 12236KB
      * 时间内存都是o（n）但是递归的代码很简单。这个思想需要继续磨练。
-     * @author hui
-     * @version 1.0
+     *
      * @param root
      * @return int
+     * @author hui
+     * @version 1.0
      * @date 2023/6/14 0:08
      */
-    public int maxDepthOfficeRecursion (TreeNode root) {
+    public int maxDepthOfficeRecursion(TreeNode root) {
         //空节点没有深度
-        if(root == null) {
+        if (root == null) {
             return 0;
         }
         //返回子树深度+1
@@ -408,20 +415,21 @@ public class TreeUtil {
      * 2.叶子节点是指没有子节点的节点
      * 3.路径只能从父节点到子节点，不能从子节点到父节点
      * 4.总节点数目为n
+     *
      * @param root TreeNode类
-     * @param sum int整型
+     * @param sum  int整型
      * @return bool布尔型
      */
-    public boolean hasPathSum (TreeNode root, int sum) {
+    public boolean hasPathSum(TreeNode root, int sum) {
         // write code here
-        if(root == null){
+        if (root == null) {
             return false;
         }
         //如果是叶子节点，且路径和为sum
-        if(root.left==null&&root.right==null&& sum-root.val==0){
+        if (root.left == null && root.right == null && sum - root.val == 0) {
             return true;
         }
-        return hasPathSum(root.left,sum-root.val) || hasPathSum(root.right,sum-root.val);
+        return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
     }
 
 
@@ -429,16 +437,16 @@ public class TreeUtil {
      * hasPathSumStack:
      * 二叉树中和为某一值的路径
      *
-     * @author hui
-     * @version 1.0
      * @param root
      * @param sum
      * @return boolean
+     * @author hui
+     * @version 1.0
      * @date 2023/6/14 23:47
      */
-    public boolean hasPathSumStack (TreeNode root, int sum) {
+    public boolean hasPathSumStack(TreeNode root, int sum) {
         //空节点找不到路径
-        if(root == null) {
+        if (root == null) {
             return false;
         }
         //栈辅助深度优先遍历
@@ -447,30 +455,32 @@ public class TreeUtil {
         Stack<Integer> s2 = new Stack<Integer>();
         s1.push(root);
         s2.push(root.val);
-        while(!s1.isEmpty()){
+        while (!s1.isEmpty()) {
             //弹出相应节点
             TreeNode temp = s1.pop();
             //弹出到该点为止的当前路径和
             int cur_sum = s2.pop();
             //叶子节点且当前路径和等于sum
-            if(temp.left == null && temp.right == null && cur_sum == sum) {
+            if (temp.left == null && temp.right == null && cur_sum == sum) {
                 return true;
             }
             //左节点及路径和入栈
-            if(temp.left != null){
+            if (temp.left != null) {
                 s1.push(temp.left);
                 s2.push(cur_sum + temp.left.val);
             }
             //右节点及路径和入栈
-            if(temp.right != null){
+            if (temp.right != null) {
                 s1.push(temp.right);
                 s2.push(cur_sum + temp.right.val);
             }
         }
         return false;
     }
+
     TreeNode head = null;
     TreeNode pre = null;
+
     /**
      * convert:
      * 二叉搜索树与双向链表
@@ -479,16 +489,17 @@ public class TreeUtil {
      * 2.返回链表中的第一个节点的指针
      * 3.函数返回的TreeNode，有左右指针，其实可以看成一个双向链表的数据结构
      * 4.你不用输出双向链表，程序会根据你的返回值自动打印输出
-     *
+     * <p>
      * 二叉查找树（Binary Search Tree），（又：二叉搜索树，二叉排序树）它或者是一棵空树，或者是具有下列性质的二叉树：
      * 若它的左子树不空，则左子树上所有结点的值均小于它的根结点的值； 若它的右子树不空，则右子树上所有结点的值均大于它的根结点的值；
      * 它的左、右子树也分别为二叉排序树。二叉搜索树作为一种经典的数据结构，
      * 它既有链表的快速插入与删除操作的特点，又有数组快速查找的优势；所以应用十分广泛，
      * 例如在文件系统和数据库系统一般会采用这种数据结构进行高效率的排序与检索操作。
+     *
+     * @param pRootOfTree
+     * @return cn.hwyee.datastructures.tree.TreeNode
      * @author hui
      * @version 1.0
-     * @param pRootOfTree  
-     * @return cn.hwyee.datastructures.tree.TreeNode
      * @date 2023/6/15 0:35
      */
     public TreeNode convert(TreeNode pRootOfTree) {
@@ -512,5 +523,137 @@ public class TreeUtil {
         return head;
     }
 
+
+    /**
+     * 对称的二叉树
+     * 给定一棵二叉树，判断其是否是自身的镜像（即：是否对称）
+     *
+     * @param pRoot TreeNode类
+     * @return bool布尔型
+     */
+    boolean isSymmetrical(TreeNode pRoot) {
+        return recursion(pRoot, pRoot);
+    }
+
+    boolean recursion(TreeNode root1, TreeNode root2) {
+        //可以两个都为空
+        if (root1 == null && root2 == null) {
+            return true;
+        }
+        //只有一个为空或者节点值不同，必定不对称
+        if (root1 == null || root2 == null || root1.val != root2.val) {
+            return false;
+        }
+        //每层对应的节点进入递归比较
+        return recursion(root1.left, root2.right) && recursion(root1.right, root2.left);
+    }
+
+    /**
+     * 合并二叉树
+     * 已知两颗二叉树，将它们合并成一颗二叉树。合并规则是：
+     * 都存在的结点，就将结点值加起来，否则空的位置就由另一个树的结点来代替。
+     *
+     * @param t1 TreeNode类
+     * @param t2 TreeNode类
+     * @return TreeNode类
+     */
+    public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+        //若只有一个节点返回另一个，两个都为null自然返回null
+        if (t1 == null) {
+            return t2;
+        }
+        if (t2 == null) {
+            return t1;
+        }
+        //根左右的方式递归
+        TreeNode head = new TreeNode(t1.val + t2.val);
+        head.left = mergeTrees(t1.left, t2.left);
+        head.right = mergeTrees(t1.right, t2.right);
+        return head;
+    }
+
+
+    /**
+     * 二叉树的镜像
+     * 操作给定的二叉树，将其变换为源来的二叉树的镜像。
+     *
+     * @param pRoot TreeNode类
+     * @return TreeNode类
+     */
+    public TreeNode Mirror(TreeNode pRoot) {
+        // write code here
+        if (pRoot == null) {
+            return null;
+        }
+
+        TreeNode left = Mirror(pRoot.left);
+        TreeNode right = Mirror(pRoot.right);
+        pRoot.left = right;
+        pRoot.right = left;
+        return pRoot;
+    }
+
+    int preVal = Integer.MIN_VALUE;
+
+    /**
+     * 判断是不是二叉搜索树
+     * 二叉搜索树是一种特殊的二叉树，它的每个节点值大于它的左子节点，且大于全部左子树的节点值，小于它右子节点，
+     * 且小于全部右子树的节点值。因此二叉搜索树一定程度上算是一种排序结构。
+     *
+     * @param root TreeNode类
+     * @return bool布尔型
+     */
+    public boolean isValidBST(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        //先进入左子树
+        if (!isValidBST(root.left)) {
+            return false;
+        }
+        if (root.val < preVal) {
+            return false;
+        }
+        //更新最值
+        preVal = root.val;
+        //再进入右子树
+        return isValidBST(root.right);
+    }
+
+    /**
+     * 判断是不是完全二叉树
+     * 完全二叉树的定义：若二叉树的深度为 h，除第 h 层外，其它各层的结点数都达到最大个数，
+     * 第 h 层所有的叶子结点都连续集中在最左边（即节点必须是从左到右连续的），这就是完全二叉树.
+     *
+     * @param root TreeNode类
+     * @return bool布尔型
+     */
+    public boolean isCompleteTree(TreeNode root) {
+        //空树一定是完全二叉树
+        if (root == null) {
+            return true;
+        }
+        //辅助队列
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        TreeNode cur;
+        //定义一个首次出现的标记位
+        boolean notComplete = false;
+        while (!queue.isEmpty()) {
+            cur = queue.poll();
+            //标记第一次遇到空节点
+            if (cur == null) {
+                notComplete = true;
+                continue;
+            }
+            //后续访问已经遇到空节点了，说明经过了叶子
+            if (notComplete) {
+                return false;
+            }
+            queue.offer(cur.left);
+            queue.offer(cur.right);
+        }
+        return true;
+    }
 
 }
