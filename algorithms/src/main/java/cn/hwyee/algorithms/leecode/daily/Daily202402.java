946b61f60935a73235b2df61777ad4a3f28f589f
hwyee <hwyee@foxmail.com>
2024-02-28 00:00:07 +0800

    LeetCode Daily: 二叉树 2867. 统计树中的合法路径数目。[扌少].

--- algorithms/src/main/java/cn/hwyee/algorithms/leecode/daily/Daily202402.java
+++ algorithms/src/main/java/cn/hwyee/algorithms/leecode/daily/Daily202402.java
@@ -29,7 +29,10 @@ public class Daily202402 {
         //priorityQueue，只能用继承了Comparable接口的类。否则报错。TreeNode cannot be cast to class java.lang.Comparable
 //        PriorityQueue<TreeNode> priorityQueue = new PriorityQueue<>();
 //        priorityQueue.add(new TreeNode(1));
-
+//        System.out.println((int)Math.sqrt(10));
+        Daily202402 daily202402 = new Daily202402();
+        Solution27_1 solution271 = new Solution27_1();
+        solution271.countPaths(499979, null);
     }
 
     /**
@@ -582,6 +585,112 @@ public class Daily202402 {
                 return rangeSumBST(root.right, low, high);
             }
             return root.val + rangeSumBST(root.left, low, high) + rangeSumBST(root.right, low, high);
+        }
+
+
+    }
+
+
+    /**
+     * 2867. 统计树中的合法路径数目: 
+     *  给你一棵 n 个节点的无向树，节点编号为 1 到 n 。给你一个整数 n 和一个长度为 n - 1 的二维整数数组 edges ，其中 edges[i] = [ui, vi] 表示节点 ui 和 vi 在树中有一条边。
+     * 请你返回树中的 合法路径数目 。
+     * 如果在节点 a 到节点 b 之间 恰好有一个 节点的编号是质数，那么我们称路径 (a, b) 是 合法的 。
+     *
+     * 注意：
+     *
+     * 路径 (a, b) 指的是一条从节点 a 开始到节点 b 结束的一个节点序列，序列中的节点 互不相同 ，且相邻节点之间在树上有一条边。
+     * 路径 (a, b) 和路径 (b, a) 视为 同一条 路径，且只计入答案 一次 。
+     *
+     * @author hui
+     * @version 1.0
+     * @param null  
+     * @return 
+     * @date 2024/2/27 22:57
+     */
+    static class Solution27_1 {
+        public long countPaths(int n, int[][] edges) {
+            //1.找出质数
+            List<Integer> list = new ArrayList<>();
+            for (int i = 2; i < n; i++) {
+                double doubleVal  = Math.sqrt(i);
+                int sqrt  = (int)(doubleVal);
+                if (sqrt == doubleVal){
+                    continue;
+                }
+                list.add(i);
+                for (int j = 2; j <= sqrt; j++) {
+                    if (i%j ==0 ){
+                        list.remove(new Integer(i));
+                        break;
+                    }
+                }
+            }
+            System.out.println(list);
+            return 1;
+        }
+        // 埃氏筛
+        private static final int N = 100001;
+        private static boolean[] isPrime = new boolean[N];
+        static {
+            Arrays.fill(isPrime, true);
+            isPrime[1] = false;
+            for (int i = 2; i * i < N; i++) {
+                if (isPrime[i]) {
+                    for (int j = i * i; j < N; j += i) {
+                        isPrime[j] = false;
+                    }
+                }
+            }
+        }
+
+        public long countPathsGF(int n, int[][] edges) {
+            List<Integer>[] G = new ArrayList[n + 1];
+            for (int i = 0; i <= n; i++) {
+                G[i] = new ArrayList<>();
+            }
+
+            for (int[] edge : edges) {
+                int i = edge[0], j = edge[1];
+                G[i].add(j);
+                G[j].add(i);
+            }
+
+            List<Integer> seen = new ArrayList<>();
+            long res = 0;
+            long[] count = new long[n + 1];
+            for (int i = 1; i <= n; i++) {
+                if (!isPrime[i]) {
+                    continue;
+                }
+                long cur = 0;
+                for (int j : G[i]) {
+                    if (isPrime[j]) {
+                        continue;
+                    }
+                    if (count[j] == 0) {
+                        seen.clear();
+                        dfs(G, seen, j, 0);
+                        long cnt = seen.size();
+                        for (int k : seen) {
+                            count[k] = cnt;
+                        }
+                    }
+                    res += count[j] * cur;
+                    cur += count[j];
+                }
+                res += cur;
+            }
+            return res;
+        }
+
+        private void dfs(List<Integer>[] G, List<Integer> seen, int i, int pre) {
+            seen.add(i);
+            for (int j : G[i]) {
+                if (j != pre && !isPrime[j]) {
+                    dfs(G, seen, j, i);
+                }
+            }
         }
 
 
