eb726f5c265732deed5eeb2a226ad420f5d3bc46
hwyee <hwyee@foxmail.com>
2024-03-03 17:24:29 +0800

    LeetCode Daily: 二叉树 225. 用队列实现栈。[完成].

--- algorithms/src/main/java/cn/hwyee/algorithms/leecode/daily/Daily202403.java
+++ algorithms/src/main/java/cn/hwyee/algorithms/leecode/daily/Daily202403.java
@@ -107,13 +107,13 @@ public class Daily202403 {
 
     /**
      * 225. 用队列实现栈:
-     请你仅使用两个队列实现一个后入先出（LIFO）的栈，并支持普通栈的全部四种操作（push、top、pop 和 empty）。
-     实现 MyStack 类：
-     void push(int x) 将元素 x 压入栈顶。
-     int pop() 移除并返回栈顶元素。
-     int top() 返回栈顶元素。
-     boolean empty() 如果栈是空的，返回 true ；否则，返回 false 。
-
+     * 请你仅使用两个队列实现一个后入先出（LIFO）的栈，并支持普通栈的全部四种操作（push、top、pop 和 empty）。
+     * 实现 MyStack 类：
+     * void push(int x) 将元素 x 压入栈顶。
+     * int pop() 移除并返回栈顶元素。
+     * int top() 返回栈顶元素。
+     * boolean empty() 如果栈是空的，返回 true ；否则，返回 false 。
+     *
      * @author hui
      * @version 1.0
      * @return
