746f9b228de3cf37104f8cd28902af30a6762fea
hwyee <hwyee@foxmail.com>
2024-03-03 17:24:29 +0800

    LeetCode Daily: 二叉树 2368. 受限条件下可到达节点的数目。[完成].
    (cherry picked from commit a3d5e186f88983c80a0869c48fdd5f648dde316a)

--- algorithms/src/main/java/cn/hwyee/algorithms/leecode/daily/Daily202403.java
+++ algorithms/src/main/java/cn/hwyee/algorithms/leecode/daily/Daily202403.java
@@ -103,8 +103,56 @@ public class Daily202403 {
                 }
             }
         }
-
-
     }
+
+    /**
+     * 225. 用队列实现栈:
+     请你仅使用两个队列实现一个后入先出（LIFO）的栈，并支持普通栈的全部四种操作（push、top、pop 和 empty）。
+     实现 MyStack 类：
+     void push(int x) 将元素 x 压入栈顶。
+     int pop() 移除并返回栈顶元素。
+     int top() 返回栈顶元素。
+     boolean empty() 如果栈是空的，返回 true ；否则，返回 false 。
+
+     * @author hui
+     * @version 1.0
+     * @return
+     * @date 2024/3/3 17:17
+     */
+    class MyStack {
+        /**
+         * Your MyStack object will be instantiated and called as such:
+         * MyStack obj = new MyStack();
+         * obj.push(x);
+         * int param_2 = obj.pop();
+         * int param_3 = obj.top();
+         * boolean param_4 = obj.empty();
+         */
+        List<Integer> list ;
+        public MyStack() {
+            list = new ArrayList<Integer>();
+        }
+
+        public void push(int x) {
+            list.add(x);
+        }
+
+        public int pop() {
+            Integer integer = list.get(list.size() - 1);
+            list.remove(list.size()-1);
+            return integer;
+        }
+
+        public int top() {
+            Integer integer = list.get(list.size() - 1);
+            return integer;
+        }
+
+        public boolean empty() {
+            return list.isEmpty();
+        }
+    }
+
+
 
 }
