00e44cf9fd61b7487b6dcd7afe05e7d046f55d57
hwyee <hwyee@foxmail.com>
2024-03-04 22:34:42 +0800

    LeetCode Daily: 二叉树 232. 用栈实现队列。[完成].

--- algorithms/src/main/java/cn/hwyee/algorithms/leecode/daily/Daily202403.java
+++ algorithms/src/main/java/cn/hwyee/algorithms/leecode/daily/Daily202403.java
@@ -4,6 +4,7 @@ import org.springframework.cglib.proxy.Factory;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 /**
  * @author hwyee@foxmail.com
@@ -16,7 +17,10 @@ import java.util.List;
 public class Daily202403 {
 
     public static void main(String[] args) {
-
+        MyQueue myQueue = new MyQueue();
+        myQueue.push(1);
+        myQueue.pop();
+        System.out.println(myQueue.empty());
     }
 
     /**
@@ -68,8 +72,9 @@ public class Daily202403 {
      * 给你一个二维整数数组 edges ，长度为 n - 1 ，其中 edges[i] = [ai, bi] 表示树中节点 ai 和 bi 之间存在一条边。另给你一个整数数组 restricted 表示 受限 节点。
      * 在不访问受限节点的前提下，返回你可以从节点 0 到达的 最多 节点数目。
      * 注意，节点 0 不 会标记为受限节点。也就是至少有一个。
-     *
+     * <p>
      * edges 表示一棵有效的树
+     *
      * @author hui
      * @version 1.0
      * @return
@@ -77,7 +82,8 @@ public class Daily202403 {
      */
     class Solution {
         int res = 0;
-        boolean[] isRestricted ;
+        boolean[] isRestricted;
+
         public int reachableNodes(int n, int[][] edges, int[] restricted) {
             isRestricted = new boolean[n];
             for (int i : restricted) {
@@ -95,10 +101,10 @@ public class Daily202403 {
             return res;
         }
 
-        public void dfs(int i, int j, ArrayList<Integer>[] edges){
+        public void dfs(int i, int j, ArrayList<Integer>[] edges) {
             res++;
             for (Integer o : edges[i]) {
-                if (o!=j && !isRestricted[o]){
+                if (o != j && !isRestricted[o]) {
                     dfs(o, i, edges);
                 }
             }
@@ -128,7 +134,8 @@ public class Daily202403 {
          * int param_3 = obj.top();
          * boolean param_4 = obj.empty();
          */
-        List<Integer> list ;
+        List<Integer> list;
+
         public MyStack() {
             list = new ArrayList<Integer>();
         }
@@ -139,7 +146,7 @@ public class Daily202403 {
 
         public int pop() {
             Integer integer = list.get(list.size() - 1);
-            list.remove(list.size()-1);
+            list.remove(list.size() - 1);
             return integer;
         }
 
@@ -153,6 +160,97 @@ public class Daily202403 {
         }
     }
 
+    /**
+     * 232. 用栈实现队列:
+     * 请你仅使用两个栈实现先入先出队列。队列应当支持一般队列支持的所有操作（push、pop、peek、empty）：
+     * <p>
+     * 实现 MyQueue 类：
+     * <p>
+     * void push(int x) 将元素 x 推到队列的末尾
+     * int pop() 从队列的开头移除并返回元素
+     * int peek() 返回队列开头的元素
+     * boolean empty() 如果队列为空，返回 true ；否则，返回 false
+     * 说明：
+     * <p>
+     * 你 只能 使用标准的栈操作 —— 也就是只有 push to top, peek/pop from top, size, 和 is empty 操作是合法的。
+     * 你所使用的语言也许不支持栈。你可以使用 list 或者 deque（双端队列）来模拟一个栈，只要是标准的栈操作即可。
+     *
+     * @author hui
+     * @version 1.0
+     * @return
+     * @date 2024/3/4 21:44
+     */
+    static class MyQueue {
+        private Stack<Integer> stack1;
+        private Stack<Integer> stack2;
+
+        /**
+         * MyQueue:
+         * <p>
+         * * Your MyQueue object will be instantiated and called as such:
+         * * MyQueue obj = new MyQueue();
+         * * obj.push(x);
+         * * int param_2 = obj.pop();
+         * * int param_3 = obj.peek();
+         * * boolean param_4 = obj.empty();
+         *
+         * @return
+         * @author hui
+         * @version 1.0
+         * @date 2024/3/4 21:44
+         */
+        public MyQueue() {
+            stack1 = new Stack();
+            stack2 = new Stack();
+        }
+
+        public void push(int x) {
+            if (stack1.isEmpty() && stack2.isEmpty()) {
+                stack1.push(x);
+            } else if (stack2.isEmpty()) {
+                while (!stack1.isEmpty()) {
+                    stack2.push(stack1.pop());
+                }
+                stack2.push(x);
+                while (!stack2.isEmpty()) {
+                    stack1.push(stack2.pop());
+                }
+            } else {
+                while (!stack2.isEmpty()) {
+                    stack1.push(stack2.pop());
+                }
+                stack1.push(x);
+                while (!stack1.isEmpty()) {
+                    stack2.push(stack1.pop());
+                }
+            }
+
+        }
+
+        public int pop() {
+            int x = 0;
+            if (stack2.isEmpty()) {
+                x = stack1.pop();
+            } else {
+                x = stack2.pop();
+            }
+            return x;
+        }
+
+        public int peek() {
+            int x = 0;
+            if (stack2.isEmpty()) {
+                x = stack1.peek();
+            } else {
+                x = stack2.peek();
+            }
+            return x;
+        }
+
+        public boolean empty() {
+            return (stack1.isEmpty() && stack2.isEmpty());
+        }
+    }
 
 
 }
