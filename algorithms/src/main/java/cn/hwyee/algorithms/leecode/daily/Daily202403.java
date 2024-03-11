34579a9ede9b069a66ad0a9c1c3a9692e57be1ef
hwyee <hwyee@foxmail.com>
2024-03-11 23:19:52 +0800

    LeetCode Daily: 字符串： 2129. 将标题首字母大写。[完成].

--- algorithms/src/main/java/cn/hwyee/algorithms/leecode/daily/Daily202403.java
+++ algorithms/src/main/java/cn/hwyee/algorithms/leecode/daily/Daily202403.java
@@ -578,9 +578,38 @@ public class Daily202403 {
             }
             return Integer.toString(bulls) + "A" + Integer.toString(cows) + "B";
         }
-
-
     }
 
+    /**
+     * 2129. 将标题首字母大写:
+     * 给你一个字符串 title ，它由单个空格连接一个或多个单词组成，每个单词都只包含英文字母。请你按以下规则将每个单词的首字母 大写 ：
+     * 如果单词的长度为 1 或者 2 ，所有字母变成小写。
+     * 否则，将单词首字母大写，剩余字母变成小写。
+     * 请你返回 大写后 的 title 。
+     *
+     * @author hui
+     * @version 1.0
+     * @return
+     * @date 2024/3/11 22:24
+     */
+    class Solution_11_1 {
+        public String capitalizeTitle(String title) {
+            String[] split = title.split(" ");
+            StringBuilder sb = new StringBuilder();
+            //97-65
+            int up = 'a' - 'A';
+            for (String s : split) {
+                if (s.length() == 1 || s.length() == 2) {
+                    sb.append(s.toLowerCase()).append(" ");
+                }else {
+                    sb.append(s.charAt(0)>96?(char) (s.charAt(0) - up):
+                            s.charAt(0)).append(s.substring(1)
+                            .toLowerCase()).append(" ");
+                }
+            }
+            sb.deleteCharAt(sb.length()-1);
+            return sb.toString();
+        }
+    }
 
 }
