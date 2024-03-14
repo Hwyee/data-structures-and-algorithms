470ce2384287db172b65b1cf67c8d2da3690235e
hwyee <hwyee@foxmail.com>
2024-03-14 23:37:13 +0800

    LeetCode Daily: 字符串： 2789. 合并后数组中的最大元素。[完成].

--- algorithms/src/main/java/cn/hwyee/algorithms/leecode/daily/Daily202403.java
+++ algorithms/src/main/java/cn/hwyee/algorithms/leecode/daily/Daily202403.java
@@ -767,6 +767,26 @@ public class Daily202403 {
         }
     }
 
-
+    /**
+     * 2789. 合并后数组中的最大元素: 
+     * 给你一个下标从 0 开始、由正整数组成的数组 nums 。
+     * 你可以在数组上执行下述操作 任意 次：
+     * 选中一个同时满足 0 <= i < nums.length - 1 和 nums[i] <= nums[i + 1] 的整数 i 。将元素 nums[i + 1] 替换为 nums[i] + nums[i + 1] ，并从数组中删除元素 nums[i] 。
+     * 返回你可以从最终数组中获得的 最大 元素的值。
+     *
+     * @author hui
+     * @version 1.0
+     * @return
+     * @date 2024/3/14 23:14
+     */
+    class Solution_14_1 {
+        public long maxArrayValue(int[] nums) {
+            long sum = nums[nums.length - 1];
+            for (int i = nums.length - 2; i >= 0; i--) {
+                sum = nums[i] <= sum ? nums[i] + sum : nums[i];
+            }
+            return sum;
+        }
+    }
 
 }
