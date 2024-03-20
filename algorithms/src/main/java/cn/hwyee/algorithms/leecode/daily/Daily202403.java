9340261baa8ce4fc3b6379d3e2c459a5b360b56b
hwyee <hwyee@foxmail.com>
2024-03-21 00:18:09 +0800

    LeetCode75: 二进制： 1969. 数组元素的最小非零乘积.[完成].

--- algorithms/src/main/java/cn/hwyee/algorithms/leecode/daily/Daily202403.java
+++ algorithms/src/main/java/cn/hwyee/algorithms/leecode/daily/Daily202403.java
@@ -1084,4 +1084,53 @@ public class Daily202403 {
 
     }
 
+    /**
+     * 1969. 数组元素的最小非零乘积: 
+     * 给你一个正整数 p 。你有一个下标从 1 开始的数组 nums ，这个数组包含范围 [1, 2的p次方 - 1] 内所有整数的二进制形式（两端都 包含）。你可以进行以下操作 任意 次：
+     * 从 nums 中选择两个元素 x 和 y  。
+     * 选择 x 中的一位与 y 对应位置的位交换。对应位置指的是两个整数 相同位置 的二进制位。
+     * 比方说，如果 x = 1101 且 y = 0011 ，交换右边数起第 2 位后，我们得到 x = 1111 和 y = 0001 。
+     * 请你算出进行以上操作 任意次 以后，nums 能得到的 最小非零 乘积。将乘积对 10的9次方 + 7 取余 后返回。
+     * 注意：答案应为取余 之前 的最小值。
+     *
+     * @author hui
+     * @version 1.0
+     * @return
+     * @date 2024/3/20 23:35
+     */
+    class Solution_20_1 {
+        /**
+         * java7新特性,等效于1000000007
+         */
+        private static final int MOD = 1_000_000_007;
+
+        private long pow(long x, int p) {
+            x %= MOD;
+            long res = 1;
+            while (p-- > 0) {
+                res = res * x % MOD;
+                x = x * x % MOD;
+            }
+            return res;
+        }
+
+        /**
+         * minNonZeroProductLS:
+         * 贪心，快速幂
+         * 两数之和不变，两数越接近，则乘积越大，需要将两数的距离尽可能拉远。
+         * 公式
+         * (2的p次方−1)⋅(2的p次方−2)的(2的p−1次方−1)次方
+         * @author 灵神
+         * @version 1.0
+         * @param p
+         * @return int
+         * @date 2024/3/20 23:42
+         */
+        public int minNonZeroProductLS(int p) {
+            long k = (1L << p) - 1;
+            return (int) (k % MOD * pow(k - 1, p - 1) % MOD);
+        }
+    }
+
+
 }
