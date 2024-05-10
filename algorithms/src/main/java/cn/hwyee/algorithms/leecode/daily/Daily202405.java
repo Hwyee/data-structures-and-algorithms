b547f293ee4ed8e9952b9c06d8b732410090ee3a
hwyee <hwyee@foxmail.com>
2024-05-10 10:41:46 +0800

    LeetCode Daily: 2960. 统计已测试设备.

--- algorithms/src/main/java/cn/hwyee/algorithms/leecode/daily/Daily202405.java
+++ algorithms/src/main/java/cn/hwyee/algorithms/leecode/daily/Daily202405.java
@@ -546,6 +546,35 @@ public class Daily202405 {
             return ans;
         }
     }
+
+    /**
+     * 2960. 统计已测试设备:
+     * 给你一个长度为 n 、下标从 0 开始的整数数组 batteryPercentages ，表示 n 个设备的电池百分比。
+     *
+     * 你的任务是按照顺序测试每个设备 i，执行以下测试操作：
+     *
+     * 如果 batteryPercentages[i] 大于 0：
+     * 增加 已测试设备的计数。
+     * 将下标在 [i + 1, n - 1] 的所有设备的电池百分比减少 1，确保它们的电池百分比 不会低于 0 ，即 batteryPercentages[j] = max(0, batteryPercentages[j] - 1)。
+     * 移动到下一个设备。
+     * 否则，移动到下一个设备而不执行任何测试。
+     * 返回一个整数，表示按顺序执行测试操作后 已测试设备 的数量。
+     * @author hui
+     * @version 1.0 
+     * @return 
+     * @date 2024/5/10 10:35
+     */
+    class Solution_10_1 {
+        public int countTestedDevices(int[] batteryPercentages) {
+            int ans = 0;
+            for(int a:batteryPercentages){
+                if(a-ans>0){
+                    ans++;
+                }
+            }
+            return ans;
+        }
+    }
 }
 
     
