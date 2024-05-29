426c5921c68c5a5647cb4fe5bad5d976c8963c2d
hwyee <hwyee@foxmail.com>
2024-05-29 14:06:21 +0800

    LeetCode Daily: 2981. 找出出现至少三次的最长特殊子字符串 I.

--- algorithms/src/main/java/cn/hwyee/algorithms/leecode/daily/Daily202405.java
+++ algorithms/src/main/java/cn/hwyee/algorithms/leecode/daily/Daily202405.java
@@ -29,9 +29,11 @@ import java.util.stream.Stream;
 public class Daily202405 {
     public static void main(String[] args) {
         Solution_1_1 solution11 = new Solution_1_1();
-        solution11.totalCost(new int[]{31, 25, 72, 79, 74, 65, 84, 91, 18, 59, 27, 9, 81, 33, 17, 58}, 11, 2);
+//        solution11.totalCost(new int[]{31, 25, 72, 79, 74, 65, 84, 91, 18, 59, 27, 9, 81, 33, 17, 58}, 11, 2);
         Solution_12_1 solution121 = new Solution_12_1();
-        System.out.println(solution121.minDays(429));
+//        System.out.println(solution121.minDays(429));
+        Solution_29_1 solution291 = new Solution_29_1();
+        System.out.println(solution291.maximumLength("biaei"));
     }
 
     /**
@@ -1569,6 +1571,124 @@ public class Daily202405 {
             }
             return ans;
         }
+    }
+
+    /**
+     * 2981. 找出出现至少三次的最长特殊子字符串 I:
+     * 给你一个仅由小写英文字母组成的字符串 s 。
+     *
+     * 如果一个字符串仅由单一字符组成，那么它被称为 特殊 字符串。例如，字符串 "abc" 不是特殊字符串，而字符串 "ddd"、"zz" 和 "f" 是特殊字符串。
+     *
+     * 返回在 s 中出现 至少三次 的 最长特殊子字符串 的长度，如果不存在出现至少三次的特殊子字符串，则返回 -1 。
+     *
+     * 子字符串 是字符串中的一个连续 非空 字符序列。
+     * @author hui
+     * @version 1.0 
+     * @return 
+     * @date 2024/5/29 12:29
+     */
+    static class Solution_29_1 {
+        public int maximumLength(String s) {
+            //最长的
+            Map<Character,List<Integer>> map = new HashMap<Character,List<Integer>>();
+            Character pre = 0;
+            int count = 0;
+            int max =-1;
+            for (char c : s.toCharArray()) {
+                if(pre!=c){
+
+                    if (count != 0 ){
+//                        max = Math.max(max, count);
+                        if(map.get(pre) != null){
+                            map.get(pre).add(count);
+                        }else{
+                            List<Integer> list = new ArrayList<Integer>();
+                            list.add(count);
+                            map.put(pre,list);
+                        }
+                    }
+                    pre=c;
+                    count=1;
+                }else{
+                    count++;
+                }
+            }
+            char c= s.charAt(s.length()-1);
+            if (count != 0 ){
+//                        max = Math.max(max, count);
+                if(map.get(c) != null){
+                    map.get(c).add(count);
+                }else{
+                    List<Integer> list = new ArrayList<Integer>();
+                    list.add(count);
+                    map.put(c,list);
+                }
+            }
+            for (Map.Entry<Character, List<Integer>> characterListEntry : map.entrySet()) {
+                int size = characterListEntry.getValue().size();
+                int tempmax = characterListEntry.getValue().stream().max(Integer::compareTo).get();
+                int min = characterListEntry.getValue().stream().min(Integer::compareTo).get();
+                if (size>=3){
+                    int maxcount = 0;
+                    int secondmax = 0;
+                    for (Integer integer : characterListEntry.getValue()) {
+                        if(maxcount==3){
+                            break;
+                        }
+                        if(integer == tempmax){
+                            maxcount++;
+                        }else if(tempmax - 1 == integer){
+                            secondmax = integer;
+                        }
+                    }
+                    if(maxcount==3){
+                        max = Math.max(max,tempmax);
+                    }else if (maxcount == 2 || secondmax !=0){
+                        max = Math.max(max,tempmax-1);
+                    }else {
+                        max = Math.max(max,tempmax-2);
+                    }
+                }else if(size==2){
+                    if(min>=(tempmax-1)){
+                        max = Math.max(max, tempmax-1);
+                    }else {
+                        max = Math.max(max, tempmax - 2);
+                    }
+
+                }else {
+                    max = Math.max(max, tempmax-2);
+                }
+            }
+
+            return max==0?-1:max;
+        }
+
+        public int maximumLength_LS(String S) {
+            char[] s = S.toCharArray();
+            List<Integer>[] groups = new ArrayList[26];
+            Arrays.setAll(groups, i -> new ArrayList<>());
+            int cnt = 0;
+            for (int i = 0; i < s.length; i++) {
+                cnt++;
+                if (i + 1 == s.length || s[i] != s[i + 1]) {
+                    groups[s[i] - 'a'].add(cnt); // 统计连续字符长度
+                    cnt = 0;
+                }
+            }
+
+            int ans = 0;
+            for (List<Integer> a : groups) {
+                if (a.isEmpty()) continue;
+                a.sort(Collections.reverseOrder());
+                a.add(0);
+                a.add(0); // 假设还有两个空串
+                ans = Math.max(ans, Math.max(a.get(0) - 2, Math.max(Math.min(a.get(0) - 1, a.get(1)), a.get(2))));
+            }
+
+            return ans > 0 ? ans : -1;
+        }
+
+
     }
 
 }
