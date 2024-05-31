2954fd05965aceab7c971ee4c9fee585a64a8b1b
hwyee <hwyee@foxmail.com>
2024-05-31 14:00:49 +0800

    LeetCode Daily: 2965. 找出缺失和重复的数字.

--- algorithms/src/main/java/cn/hwyee/algorithms/leecode/daily/Daily202405.java
+++ algorithms/src/main/java/cn/hwyee/algorithms/leecode/daily/Daily202405.java
@@ -1691,6 +1691,40 @@ public class Daily202405 {
 
     }
 
+    /**
+     * 2965. 找出缺失和重复的数字:
+     * 给你一个下标从 0 开始的二维整数矩阵 grid，大小为 n * n ，其中的值在 [1, n2] 范围内。除了 a 出现 两次，b 缺失 之外，每个整数都 恰好出现一次 。
+     *
+     * 任务是找出重复的数字a 和缺失的数字 b 。
+     *
+     * 返回一个下标从 0 开始、长度为 2 的整数数组 ans ，其中 ans[0] 等于 a ，ans[1] 等于 b 。
+     * @author hui
+     * @version 1.0 
+     * @return 
+     * @date 2024/5/31 13:10
+     */
+    class Solution_31_1 {
+        public int[] findMissingAndRepeatedValues(int[][] grid) {
+            int[] ans = new int[2];
+            int[] all = new int[grid.length*grid.length+1];
+            int gsum = 0;
+            int sum = 0;
+            int c = 0;
+            for(int i = 0;i<grid.length;i++){
+                for(int j = 0;j<grid[0].length;j++){
+                    gsum+=grid[i][j];
+                    sum += ++c;
+                    if(all[grid[i][j]] == 1){
+                        ans[0] = grid[i][j];
+                    }
+                    all[grid[i][j]] = 1;
+                }
+            }
+            ans[1] = sum+ans[0] - gsum;
+            return ans;
+        }
+    }
+
 }
 
     
