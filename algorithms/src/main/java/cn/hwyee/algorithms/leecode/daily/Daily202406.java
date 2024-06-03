8e172e53fe5736320a1c86cb051cdd575cea6b15
hwyee <hwyee@foxmail.com>
2024-06-03 21:32:56 +0800

    LeetCode Daily: 1103. 分糖果 II

--- algorithms/src/main/java/cn/hwyee/algorithms/leecode/daily/Daily202406.java
+++ algorithms/src/main/java/cn/hwyee/algorithms/leecode/daily/Daily202406.java
@@ -81,4 +81,39 @@ public class Daily202406 {
         }
     }
 
+    /**
+     * 1103. 分糖果 II:
+     * 排排坐，分糖果。
+     *
+     * 我们买了一些糖果 candies，打算把它们分给排好队的 n = num_people 个小朋友。
+     *
+     * 给第一个小朋友 1 颗糖果，第二个小朋友 2 颗，依此类推，直到给最后一个小朋友 n 颗糖果。
+     *
+     * 然后，我们再回到队伍的起点，给第一个小朋友 n + 1 颗糖果，第二个小朋友 n + 2 颗，依此类推，直到给最后一个小朋友 2 * n 颗糖果。
+     *
+     * 重复上述过程（每次都比上一次多给出一颗糖果，当到达队伍终点后再次从队伍起点开始），直到我们分完所有的糖果。注意，就算我们手中的剩下糖果数不够（不比前一次发出的糖果多），这些糖果也会全部发给当前的小朋友。
+     *
+     * 返回一个长度为 num_people、元素之和为 candies 的数组，以表示糖果的最终分发情况（即 ans[i] 表示第 i 个小朋友分到的糖果数）。
+     * @author hui
+     * @version 1.0 
+     * @return 
+     * @date 2024/6/3 21:26
+     */
+    class Solution_3_1 {
+        public int[] distributeCandies(int candies, int num_people) {
+            int i = 1;
+            int sum = 1;
+            int[] ans = new int[num_people];
+            int j = 0;
+            for(;sum < candies;i++,sum+=i,j++){
+                ans[j%num_people] = ans[j%num_people] + i;
+            }
+            if(sum > candies ){
+                i = i-(sum - candies);
+            }
+            ans[j%num_people] = ans[j%num_people] + i;
+            return ans;
+        }
+    }
+
 }
