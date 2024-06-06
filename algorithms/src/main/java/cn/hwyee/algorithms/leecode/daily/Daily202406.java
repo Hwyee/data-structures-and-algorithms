40ede2a6e01ae7d9a5b25ea469bdace4d1d6044a
hwyee <hwyee@foxmail.com>
2024-06-06 20:49:37 +0800

    LeetCode Daily: 2938. 区分黑球与白球

--- algorithms/src/main/java/cn/hwyee/algorithms/leecode/daily/Daily202406.java
+++ algorithms/src/main/java/cn/hwyee/algorithms/leecode/daily/Daily202406.java
@@ -2,7 +2,11 @@ package cn.hwyee.algorithms.leecode.daily;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author hwyee@foxmail.com
@@ -16,7 +20,8 @@ import java.util.Arrays;
 public class Daily202406 {
 
     public static void main(String[] args) {
-
+        Solution_6_1 solution61 = new Solution_6_1();
+        solution61.minimumSteps("0111");
     }
 
     /**
@@ -115,5 +120,216 @@ public class Daily202406 {
             return ans;
         }
     }
+
+    /**
+     * 3067. 在带权树网络中统计可连接服务器对数目:
+     * 给你一棵无根带权树，树中总共有 n 个节点，分别表示 n 个服务器，服务器从 0 到 n - 1 编号。同时给你一个数组 edges ，
+     * 其中 edges[i] = [ai, bi, weighti] 表示节点 ai 和 bi 之间有一条双向边，边的权值为 weighti 。再给你一个整数 signalSpeed 。
+     *
+     * 如果两个服务器 a ，b 和 c 满足以下条件，那么我们称服务器 a 和 b 是通过服务器 c 可连接的 ：
+     *
+     * a < b ，a != c 且 b != c 。
+     * 从 c 到 a 的距离是可以被 signalSpeed 整除的。
+     * 从 c 到 b 的距离是可以被 signalSpeed 整除的。
+     * 从 c 到 b 的路径与从 c 到 a 的路径没有任何公共边。
+     * 请你返回一个长度为 n 的整数数组 count ，其中 count[i] 表示通过服务器 i 可连接 的服务器对的 数目 。
+     * @author hui
+     * @version 1.0 
+     * @return 
+     * @date 2024/6/5 9:25
+     */
+    class Solution_4_1 {
+        public int[] countPairsOfConnectableServersGF(int[][] edges, int signalSpeed) {
+            int n = edges.length + 1;
+            List<int[]>[] graph = new ArrayList[n];
+            Arrays.setAll(graph, i -> new ArrayList<>());
+
+            for (int[] e : edges) {
+                int u = e[0];
+                int v = e[1];
+                int w = e[2];
+                graph[u].add(new int[]{v, w});
+                graph[v].add(new int[]{u, w});
+            }
+
+            int[] res = new int[n];
+            for (int i = 0; i < n; i++) {
+                int pre = 0;
+                for (int[] e : graph[i]) {
+                    int cnt = dfs(e[0], i, e[1] % signalSpeed, signalSpeed, graph);
+                    res[i] += pre * cnt;
+                    pre += cnt;
+                }
+            }
+            return res;
+        }
+
+        private int dfs(int p, int root, int curr, int signalSpeed, List<int[]>[] graph) {
+            int res = 0;
+            if (curr == 0) {
+                res++;
+            }
+            for (int[] e : graph[p]) {
+                int v = e[0];
+                int cost = e[1];
+                if (v != root) {
+                    res += dfs(v, p, (curr + cost) % signalSpeed, signalSpeed, graph);
+                }
+            }
+            return res;
+        }
+    }
+
+
+    class BinaryIndexedTree {
+        private int[] tree;
+
+        public BinaryIndexedTree(int n) {
+            tree = new int[n + 1];
+        }
+
+        public void add(int i) {
+            while (i < tree.length) {
+                tree[i]++;
+                i += i & -i;
+            }
+        }
+
+        public int get(int i) {
+            int sum = 0;
+            while (i > 0) {
+                sum += tree[i];
+                i -= i & -i;
+            }
+            return sum;
+        }
+    }
+
+    
+    /**
+     * 3072. 将元素分配到两个数组中 II:
+     * 给你一个下标从 1 开始、长度为 n 的整数数组 nums 。
+     *
+     * 现定义函数 greaterCount ，使得 greaterCount(arr, val) 返回数组 arr 中 严格大于 val 的元素数量。
+     *
+     * 你需要使用 n 次操作，将 nums 的所有元素分配到两个数组 arr1 和 arr2 中。在第一次操作中，将 nums[1] 追加到 arr1 。在第二次操作中，将 nums[2] 追加到 arr2 。之后，在第 i 次操作中：
+     *
+     * 如果 greaterCount(arr1, nums[i]) > greaterCount(arr2, nums[i]) ，将 nums[i] 追加到 arr1 。
+     * 如果 greaterCount(arr1, nums[i]) < greaterCount(arr2, nums[i]) ，将 nums[i] 追加到 arr2 。
+     * 如果 greaterCount(arr1, nums[i]) == greaterCount(arr2, nums[i]) ，将 nums[i] 追加到元素数量较少的数组中。
+     * 如果仍然相等，那么将 nums[i] 追加到 arr1 。
+     * 连接数组 arr1 和 arr2 形成数组 result 。例如，如果 arr1 == [1,2,3] 且 arr2 == [4,5,6] ，那么 result = [1,2,3,4,5,6] 。
+     *
+     * 返回整数数组 result 。
+     * @author hui
+     * @version 1.0 
+     * @return 
+     * @date 2024/6/5 23:42
+     */
+    class Solution_5_1 {
+        public int[] resultArrayGF(int[] nums) {
+            int n = nums.length;
+            int[] sortedNums = Arrays.copyOf(nums, n);
+            Arrays.sort(sortedNums);
+
+            Map<Integer, Integer> index = new HashMap<>();
+            for (int i = 0; i < n; i++) {
+                index.put(sortedNums[i], i + 1);
+            }
+
+            List<Integer> arr1 = new ArrayList<>(List.of(nums[0]));
+            List<Integer> arr2 = new ArrayList<>(List.of(nums[1]));
+            BinaryIndexedTree tree1 = new BinaryIndexedTree(n);
+            BinaryIndexedTree tree2 = new BinaryIndexedTree(n);
+            tree1.add(index.get(nums[0]));
+            tree2.add(index.get(nums[1]));
+
+            for (int i = 2; i < n; i++) {
+                int count1 = arr1.size() - tree1.get(index.get(nums[i]));
+                int count2 = arr2.size() - tree2.get(index.get(nums[i]));
+                if (count1 > count2 || (count1 == count2 && arr1.size() <= arr2.size())) {
+                    arr1.add(nums[i]);
+                    tree1.add(index.get(nums[i]));
+                } else {
+                    arr2.add(nums[i]);
+                    tree2.add(index.get(nums[i]));
+                }
+            }
+
+            int i = 0;
+            for (int a: arr1) {
+                nums[i++] = a;
+            }
+            for (int a: arr2) {
+                nums[i++] = a;
+            }
+            return nums;
+        }
+    }
+
+    /**
+     * 2938. 区分黑球与白球:
+     * 桌子上有 n 个球，每个球的颜色不是黑色，就是白色。
+     *
+     * 给你一个长度为 n 、下标从 0 开始的二进制字符串 s，其中 1 和 0 分别代表黑色和白色的球。
+     *
+     * 在每一步中，你可以选择两个相邻的球并交换它们。
+     *
+     * 返回「将所有黑色球都移到右侧，所有白色球都移到左侧所需的 最小步数」。
+     * @author hui
+     * @version 1.0 
+     * @return 
+     * @date 2024/6/6 20:03
+     */
+    static class Solution_6_1 {
+        public long minimumSteps(String s) {
+            char[] charArray = s.toCharArray();
+
+            long blackCount = 0;
+            long blackIndex = 0;
+            for (int i = 0; i < charArray.length; i++) {
+                if (charArray[i] == '1'){
+                    blackCount++;blackIndex+=i;
+                }
+
+            }
+            long realBlackIndex = (s.length() * 2L - blackCount - 1)  * blackCount / 2 ;
+            return realBlackIndex - blackIndex;
+//            while (flag){
+//                int temp = ans;
+//                firstBB = false;
+//                for (int i = begin; i < charArray.length-1; i++) {
+//                    if (charArray[i] == '1' && charArray[i+1] == '0'){
+//                        charArray[i] = '0';
+//                        charArray[i+1] = '1';
+//                        ans++;
+//                    }else if ( !firstBB && charArray[i] == '1' && charArray[i+1] == '1' ){
+//                        begin=i;
+//                        firstBB = true;
+//                    }else if (firstBB && charArray[i] == '0' && charArray[i+1] == '0' ){
+//                        i++;
+//                    }
+//                }
+//                if (temp == ans){
+//                    flag = false;
+//                }
+//            }
+//            return ans;
+        }
+
+        public long minimumStepsGF(String s) {
+            int n = s.length();
+            long count = 0;
+            int index = 0;
+            for(int i = 0; i < n; i++){
+                if(s.charAt(i) == '0'){
+                    count += (i - index);
+                    index++;
+                }
+            }
+            return count;
+        }
+    }
+
 
 }
