30314657d81f09dc4c822d289e116a4267ccbf10
hwyee <hwyee@foxmail.com>
2024-06-12 20:47:49 +0800

    LeetCode Daily: 2806. 取整购买后的账户余额

--- algorithms/src/main/java/cn/hwyee/algorithms/leecode/daily/Daily202406.java
+++ algorithms/src/main/java/cn/hwyee/algorithms/leecode/daily/Daily202406.java
@@ -586,4 +586,29 @@ public class Daily202406 {
         }
     }
 
+    /**
+     * 2806. 取整购买后的账户余额:
+     * 一开始，你的银行账户里有 100 块钱。
+     *
+     * 给你一个整数purchaseAmount ，它表示你在一次购买中愿意支出的金额。
+     *
+     * 在一个商店里，你进行一次购买，实际支出的金额会向 最近 的 10 的 倍数 取整。换句话说，你实际会支付一个 非负 金额 roundedAmount ，满足 roundedAmount 是 10 的倍数且 abs(roundedAmount - purchaseAmount) 的值 最小 。
+     *
+     * 如果存在多于一个最接近的 10 的倍数，较大的倍数 是你的实际支出金额。
+     *
+     * 请你返回一个整数，表示你在愿意支出金额为 purchaseAmount 块钱的前提下，购买之后剩下的余额。
+     *
+     * 注意： 0 也是 10 的倍数。
+     * @author hui
+     * @version 1.0 
+     * @return 
+     * @date 2024/6/12 20:47
+     */
+    class Solution_12_1 {
+        public int accountBalanceAfterPurchase(int purchaseAmount) {
+            int i = purchaseAmount%10;
+            return i<5?100-purchaseAmount+i:90-purchaseAmount+i;
+        }
+    }
+
 }
