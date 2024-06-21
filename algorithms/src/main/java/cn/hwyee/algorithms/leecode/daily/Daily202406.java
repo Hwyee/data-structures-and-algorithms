17d7396583908fb32aaa7765e41d2a92efcae2ab
hwyee <hwyee@foxmail.com>
2024-06-21 12:35:23 +0800

    LeetCode Daily: 2748. 美丽下标对的数目

--- algorithms/src/main/java/cn/hwyee/algorithms/leecode/daily/Daily202406.java
+++ algorithms/src/main/java/cn/hwyee/algorithms/leecode/daily/Daily202406.java
@@ -988,6 +988,43 @@ public class Daily202406 {
         }
     }
 
+    /**
+     * LCP 61. 气温变化趋势:
+     * 力扣城计划在两地设立「力扣嘉年华」的分会场，气象小组正在分析两地区的气温变化趋势，对于第 i ~ (i+1) 天的气温变化趋势，将根据以下规则判断：
+     *
+     * 若第 i+1 天的气温 高于 第 i 天，为 上升 趋势
+     * 若第 i+1 天的气温 等于 第 i 天，为 平稳 趋势
+     * 若第 i+1 天的气温 低于 第 i 天，为 下降 趋势
+     * 已知 temperatureA[i] 和 temperatureB[i] 分别表示第 i 天两地区的气温。 组委会希望找到一段天数尽可能多，且两地气温变化趋势相同的时间举办嘉年华活动。请分析并返回两地气温变化趋势相同的最大连续天数。
+     * @author hui
+     * @version 1.0 
+     * @return 
+     * @date 2024/6/21 12:35
+     */
+    class Solution_21_1 {
+        public int temperatureTrend(int[] temperatureA, int[] temperatureB) {
+            int ans = 0;
+            int max = 0;
+            for(int i = 0;i<temperatureA.length-1;i++){
+                if(judge(temperatureA[i+1],temperatureA[i])==judge(temperatureB[i+1],temperatureB[i])){
+                    ans++;
+                    max=Math.max(ans,max);
+                }else{
+                    ans=0;
+                }
+            }
+            return max;
+        }
+        public int judge(int x,int y){
+            if(x>y){
+                return -1;
+            }else if(x==y){
+                return 0;
+            }else{
+                return 1;
+            }
+        }
+    }
 
 
 
