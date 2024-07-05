01aa566e1752f87943aef501d3c2800c49364f9a
hwyee <hwyee@foxmail.com>
2024-07-05 23:42:28 +0800

    LeetCode Daily: 3033. 修改矩阵

--- algorithms/src/main/java/cn/hwyee/algorithms/leecode/daily/Daily202407.java
+++ algorithms/src/main/java/cn/hwyee/algorithms/leecode/daily/Daily202407.java
@@ -147,4 +147,97 @@ public class Daily202407 {
 
 
     }
+
+
+    /**
+     * 3086. 拾起 K 个 1 需要的最少行动次数:
+     * 给你一个下标从 0 开始的二进制数组 nums，其长度为 n ；另给你一个 正整数 k 以及一个 非负整数 maxChanges 。
+     *
+     * Alice 在玩一个游戏，游戏的目标是让 Alice 使用 最少 数量的 行动 次数从 nums 中拾起 k 个 1 。游戏开始时，Alice 可以选择数组 [0, n - 1] 范围内的任何索引 aliceIndex 站立。如果 nums[aliceIndex] == 1 ，Alice 会拾起一个 1 ，并且 nums[aliceIndex] 变成0（这 不算 作一次行动）。之后，Alice 可以执行 任意数量 的 行动（包括零次），在每次行动中 Alice 必须 恰好 执行以下动作之一：
+     *
+     * 选择任意一个下标 j != aliceIndex 且满足 nums[j] == 0 ，然后将 nums[j] 设置为 1 。这个动作最多可以执行 maxChanges 次。
+     * 选择任意两个相邻的下标 x 和 y（|x - y| == 1）且满足 nums[x] == 1, nums[y] == 0 ，然后交换它们的值（将 nums[y] = 1 和 nums[x] = 0）。如果 y == aliceIndex，在这次行动后 Alice 拾起一个 1 ，并且 nums[y] 变成 0 。
+     * 返回 Alice 拾起 恰好 k 个 1 所需的 最少 行动次数。
+     * @author hui
+     * @version 1.0 
+     * @return 
+     * @date 2024/7/5 23:32
+     */
+    class Solutiond4q1 {
+        public long minimumMoves(int[] nums, int k, int maxChanges) {
+            List<Integer> pos = new ArrayList<>();
+            int c = 0; // nums 中连续的 1 长度
+            for (int i = 0; i < nums.length; i++) {
+                if (nums[i] == 0) continue;
+                pos.add(i); // 记录 1 的位置
+                c = Math.max(c, 1);
+                if (i > 0 && nums[i - 1] == 1) {
+                    if (i > 1 && nums[i - 2] == 1) {
+                        c = 3; // 有 3 个连续的 1
+                    } else {
+                        c = Math.max(c, 2); // 有 2 个连续的 1
+                    }
+                }
+            }
+
+            c = Math.min(c, k);
+            if (maxChanges >= k - c) {
+                // 其余 k-c 个 1 可以全部用两次操作得到
+                return Math.max(c - 1, 0) + (k - c) * 2;
+            }
+
+            int n = pos.size();
+            long[] sum = new long[n + 1];
+            for (int i = 0; i < n; i++) {
+                sum[i + 1] = sum[i] + pos.get(i);
+            }
+
+            long ans = Long.MAX_VALUE;
+            // 除了 maxChanges 个数可以用两次操作得到，其余的 1 只能一步步移动到 pos[i]
+            int size = k - maxChanges;
+            for (int right = size; right <= n; right++) {
+                // s1+s2 是 j 在 [left, right) 中的所有 pos[j] 到 index=pos[(left+right)/2] 的距离之和
+                int left = right - size;
+                int i = left + size / 2;
+                long index = pos.get(i);
+                long s1 = index * (i - left) - (sum[i] - sum[left]);
+                long s2 = sum[right] - sum[i] - index * (right - i);
+                ans = Math.min(ans, s1 + s2);
+            }
+            return ans + maxChanges * 2;
+        }
+
+
+    }
+
+    /**
+     * 3033. 修改矩阵:
+     * 给你一个下标从 0 开始、大小为 m x n 的整数矩阵 matrix ，新建一个下标从 0 开始、名为 answer 的矩阵。使 answer 与 matrix 相等，接着将其中每个值为 -1 的元素替换为所在列的 最大 元素。
+     *
+     * 返回矩阵 answer 。
+     * @author hui
+     * @version 1.0 
+     * @return 
+     * @date 2024/7/5 23:34
+     */
+    class Solution {
+        public int[][] modifiedMatrix(int[][] matrix) {
+            int[][] ans = new int [matrix.length][matrix[0].length];
+
+            for (int i = 0; i < matrix[0].length; i++) {
+                int max = -1;
+                for (int j = 0; j < matrix.length; j++) {
+                    max = Math.max(max, matrix[j][i]);
+                }
+                for (int j = 0; j < matrix.length; j++) {
+                    if (matrix[j][i] == -1) {
+                        ans[j][i] = max;
+                    }else{
+                        ans[j][i] = matrix[j][i];
+                    }
+                }
+            }
+            return ans;
+        }
+    }
 }
